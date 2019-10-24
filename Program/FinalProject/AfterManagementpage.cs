a0074dfe3d0f21bab55395c853ac049593269d6e
jorgecostalol <36493770+jorgecostalol@users.noreply.github.com>
2019-10-24 12:19:19 +0100

    correct file location
    Co-Authored-By: Flávio Dinis de Oliveira Figueiredo Lopes da Silva <DinisSkizee@users.noreply.github.com>

--- Program/FinalProject/AfterManagementpage.cs
+++ Program/FinalProject/AfterManagementpage.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,30 +23,3 @@ namespace FinalProject
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace FinalProject
-{
-    public partial class AfterManagementpage : Form
-    {
-        public AfterManagementpage()
-        {
-            InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
-}
->>>>>>> 18d7a0669b8e0290e33828af483b5b7f63dd98ed
--- Program/FinalProject/Management.cs
+++ Program/FinalProject/Management.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -29,35 +28,3 @@ namespace FinalProject
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace FinalProject
-{
-    public partial class Management : System.Windows.Forms.Form
-    {
-        public Management()
-        {
-            InitializeComponent();
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-    }
-}
->>>>>>> 18d7a0669b8e0290e33828af483b5b7f63dd98ed
