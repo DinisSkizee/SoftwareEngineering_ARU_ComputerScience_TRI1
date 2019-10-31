6966d070f8e4b831dd6f683e6cb1a112a2d7df71
DinisSkizee <dinissilvauk@users.noreply.github.com>
2019-10-31 14:00:00 +0000

    a

--- Program/FinalProject/SocketSelection.Designer.cs
+++ Program/FinalProject/SocketSelection.Designer.cs
@@ -679,7 +679,7 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = global::FinalProject.Properties.Resources._1437242;
+            this.pictureBox2.Image = global::FinalProject.Properties.Resources.termpng;
             this.pictureBox2.Location = new System.Drawing.Point(35, 41);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(226, 187);
