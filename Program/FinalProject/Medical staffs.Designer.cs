97f5c2e4b353a8be1c6429d805cce8b803943e45
Maryam Modibbo <Maryam Modibbo@DESKTOP-E6G8A4M>
2019-11-09 16:54:12 +0100

    med staff added

--- Program/FinalProject/Medical staffs.Designer.cs
+++ Program/FinalProject/Medical staffs.Designer.cs
@@ -143,6 +143,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Close Form";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Medical_staffs
             // 
