1ce2215c864494d749deffa04b16063c9220aee0
Pri Pri <Pri Pri@DESKTOP-CHG7KB9>
2019-12-01 11:16:36 -0800

    Changes in RD Logs GUI (no issues found)

--- Program/FinalProject/RDLogs.Designer.cs
+++ Program/FinalProject/RDLogs.Designer.cs
@@ -29,30 +29,25 @@
         private void InitializeComponent()
         {
             this.PanelTop = new System.Windows.Forms.Panel();
+            this.CloseButtonRDLogs = new System.Windows.Forms.PictureBox();
+            this.MinimizeButtonRDLogs = new System.Windows.Forms.PictureBox();
+            this.BackButtonRDLogs = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.IDTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.BackButtonRDLogs = new System.Windows.Forms.PictureBox();
-            this.MinimizeButtonRDLogs = new System.Windows.Forms.PictureBox();
-            this.CloseButtonRDLogs = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ActionComboBox = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.PanelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButtonRDLogs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizeButtonRDLogs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackButtonRDLogs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.BackButtonRDLogs)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.MinimizeButtonRDLogs)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.CloseButtonRDLogs)).BeginInit();
             this.SuspendLayout();
             // 
             // PanelTop
@@ -68,54 +63,56 @@
             this.PanelTop.Size = new System.Drawing.Size(859, 44);
             this.PanelTop.TabIndex = 8;
             // 
+            // CloseButtonRDLogs
+            // 
+            this.CloseButtonRDLogs.Image = global::FinalProject.Properties.Resources.closewhite;
+            this.CloseButtonRDLogs.Location = new System.Drawing.Point(833, 12);
+            this.CloseButtonRDLogs.Name = "CloseButtonRDLogs";
+            this.CloseButtonRDLogs.Size = new System.Drawing.Size(23, 25);
+            this.CloseButtonRDLogs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.CloseButtonRDLogs.TabIndex = 2;
+            this.CloseButtonRDLogs.TabStop = false;
+            this.CloseButtonRDLogs.Click += new System.EventHandler(this.CloseButtonRDLogs_Click);
+            // 
+            // MinimizeButtonRDLogs
+            // 
+            this.MinimizeButtonRDLogs.Image = global::FinalProject.Properties.Resources.minimizewhite;
+            this.MinimizeButtonRDLogs.Location = new System.Drawing.Point(804, 12);
+            this.MinimizeButtonRDLogs.Name = "MinimizeButtonRDLogs";
+            this.MinimizeButtonRDLogs.Size = new System.Drawing.Size(23, 25);
+            this.MinimizeButtonRDLogs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MinimizeButtonRDLogs.TabIndex = 1;
+            this.MinimizeButtonRDLogs.TabStop = false;
+            this.MinimizeButtonRDLogs.Click += new System.EventHandler(this.MinimizeButtonRDLogs_Click);
+            // 
+            // BackButtonRDLogs
+            // 
+            this.BackButtonRDLogs.Image = global::FinalProject.Properties.Resources._8B7gpJyx_400x400;
+            this.BackButtonRDLogs.Location = new System.Drawing.Point(4, 9);
+            this.BackButtonRDLogs.Name = "BackButtonRDLogs";
+            this.BackButtonRDLogs.Size = new System.Drawing.Size(34, 28);
+            this.BackButtonRDLogs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BackButtonRDLogs.TabIndex = 0;
+            this.BackButtonRDLogs.TabStop = false;
+            this.BackButtonRDLogs.Click += new System.EventHandler(this.BackButtonRDLogs_Click);
+            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5});
             this.dataGridView1.Location = new System.Drawing.Point(297, 61);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(551, 309);
             this.dataGridView1.TabIndex = 7;
             // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Staff ID";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Date";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Time";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Register";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Deregister";
-            this.Column5.Name = "Column5";
-            // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.ActionComboBox);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.IDTextBox);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -146,92 +143,57 @@
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // textBox2
+            // IDTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(86, 81);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(86, 40);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 4;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(86, 123);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(70, 17);
-            this.radioButton2.TabIndex = 3;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Register ";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(162, 123);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(78, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Deregister";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.IDTextBox.Location = new System.Drawing.Point(81, 40);
+            this.IDTextBox.Name = "IDTextBox";
+            this.IDTextBox.Size = new System.Drawing.Size(135, 22);
+            this.IDTextBox.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.Size = new System.Drawing.Size(18, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Staff ID";
+            this.label2.Text = "ID";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Date";
+            this.label1.Text = "DATE";
             // 
-            // BackButtonRDLogs
+            // label3
             // 
-            this.BackButtonRDLogs.Image = global::FinalProject.Properties.Resources._8B7gpJyx_400x400;
-            this.BackButtonRDLogs.Location = new System.Drawing.Point(4, 9);
-            this.BackButtonRDLogs.Name = "BackButtonRDLogs";
-            this.BackButtonRDLogs.Size = new System.Drawing.Size(34, 28);
-            this.BackButtonRDLogs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.BackButtonRDLogs.TabIndex = 0;
-            this.BackButtonRDLogs.TabStop = false;
-            this.BackButtonRDLogs.Click += new System.EventHandler(this.BackButtonRDLogs_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ACTION";
             // 
-            // MinimizeButtonRDLogs
+            // ActionComboBox
             // 
-            this.MinimizeButtonRDLogs.Image = global::FinalProject.Properties.Resources.minimizewhite;
-            this.MinimizeButtonRDLogs.Location = new System.Drawing.Point(804, 12);
-            this.MinimizeButtonRDLogs.Name = "MinimizeButtonRDLogs";
-            this.MinimizeButtonRDLogs.Size = new System.Drawing.Size(23, 25);
-            this.MinimizeButtonRDLogs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.MinimizeButtonRDLogs.TabIndex = 1;
-            this.MinimizeButtonRDLogs.TabStop = false;
-            this.MinimizeButtonRDLogs.Click += new System.EventHandler(this.MinimizeButtonRDLogs_Click);
+            this.ActionComboBox.FormattingEnabled = true;
+            this.ActionComboBox.Items.AddRange(new object[] {
+            "REGISTER ",
+            "DEREGISTER"});
+            this.ActionComboBox.Location = new System.Drawing.Point(81, 119);
+            this.ActionComboBox.Name = "ActionComboBox";
+            this.ActionComboBox.Size = new System.Drawing.Size(135, 21);
+            this.ActionComboBox.TabIndex = 10;
             // 
-            // CloseButtonRDLogs
+            // dateTimePicker1
             // 
-            this.CloseButtonRDLogs.Image = global::FinalProject.Properties.Resources.closewhite;
-            this.CloseButtonRDLogs.Location = new System.Drawing.Point(833, 12);
-            this.CloseButtonRDLogs.Name = "CloseButtonRDLogs";
-            this.CloseButtonRDLogs.Size = new System.Drawing.Size(23, 25);
-            this.CloseButtonRDLogs.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.CloseButtonRDLogs.TabIndex = 2;
-            this.CloseButtonRDLogs.TabStop = false;
-            this.CloseButtonRDLogs.Click += new System.EventHandler(this.CloseButtonRDLogs_Click);
+            this.dateTimePicker1.Location = new System.Drawing.Point(81, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(135, 22);
+            this.dateTimePicker1.TabIndex = 11;
             // 
             // RDLogs
             // 
@@ -245,12 +207,12 @@
             this.Name = "RDLogs";
             this.Text = "RDLogs";
             this.PanelTop.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButtonRDLogs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizeButtonRDLogs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackButtonRDLogs)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.BackButtonRDLogs)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.MinimizeButtonRDLogs)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.CloseButtonRDLogs)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -259,22 +221,17 @@
 
         private System.Windows.Forms.Panel PanelTop;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox IDTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox CloseButtonRDLogs;
         private System.Windows.Forms.PictureBox MinimizeButtonRDLogs;
         private System.Windows.Forms.PictureBox BackButtonRDLogs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox ActionComboBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
