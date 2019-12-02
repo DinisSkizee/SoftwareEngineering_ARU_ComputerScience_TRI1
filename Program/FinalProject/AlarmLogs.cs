b52f0319d9a8593e93abba6dcc813609db61983b
Pri Pri <Pri Pri@DESKTOP-CHG7KB9>
2019-12-01 16:07:16 -0800

    is database working? (no issues found)

--- Program/FinalProject/AlarmLogs.cs
+++ Program/FinalProject/AlarmLogs.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace FinalProject
 {
     public partial class AlarmLogs : Form
@@ -16,19 +16,29 @@ namespace FinalProject
         {
             InitializeComponent();
         }
-
+        //to minimize the page
         private void MinimizeButtonAlarmLogs_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
 
         }
-
+        // to exit the page
         private void CloseButtonAlarmLogs_Click(object sender, EventArgs e)
         {
             Application.Exit();
 
         }
+
+        /* to call the database connection 
+        * declaring the varriable name
+        */
+
+        SqlConnection CON = new SqlConnection("Data Source=.;Initial Catalog=Datainfo;Integrated Security=True;Pooling=False");
         // backbutton leads the page back to Logs page
+
+
+
+
         private void BackButton_Click(object sender, EventArgs e)
         {
             Logs logs = new Logs();
@@ -36,8 +46,34 @@ namespace FinalProject
             logs.Location = this.Location;
             this.Hide();
         }
+        // saves data entered
+        private void AlarmLogsSaveButton_Click(object sender, EventArgs e)
+        {
+            CON.Open();
+            SqlDataAdapter SDA = new SqlDataAdapter("INSERT INTO ALARMLOGS (ID,DATE,FIRSTALARM,ALARMRECTIFIED,COMMENTS) VALUES ('" + IDBox.Text + "','" + DateBox.Text + "', '" + FirstBox.Text + "', '" + LastBox.Text + "','" + CommentsBox.Text + "')", CON);
+            SDA.SelectCommand.ExecuteNonQuery();
+            CON.Close();
+            MessageBox.Show("Saved");
+        }
+        // to view data entered 
+        private void AlarmLogsView_Click(object sender, EventArgs e)
+        {
+            CON.Open();
+            SqlDataAdapter SDA = new SqlDataAdapter("SELECT * FROM ALARMLOGS", CON);
+            DataTable DATA = new DataTable();
+            SDA.Fill(DATA);
+            dataGridView1.DataSource = DATA;
+            CON.Close();
+        }
 
-        
+        private void AlarmDeleteButton_Click(object sender, EventArgs e)
+        {
+            CON.Open();
+            SqlDataAdapter SDA = new SqlDataAdapter("DELETE FROM ALARMLOGS WHERE ID='" + IDBox.Text + "'", CON);
+            SDA.SelectCommand.ExecuteNonQuery();
+            CON.Close();
+            MessageBox.Show("Deleted");
+        }
     }
    
 
